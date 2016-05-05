76188855b43fce456ef32255cac95240451feafc
Meghan <shannon.mss@gmail.com>
2016-05-04 18:20:14 -0600

    added a link to getting to know you, added whoyouare.html,  updated gettingtoknowyou index page
    d

--- assignments/gettingtoknowyou/index.php
+++ assignments/gettingtoknowyou/index.php
@@ -1,6 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php
+$action = filter_input(INPUT_POST,'action');
 
+if ($action == NULL || $action == "") {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        require ('../views/gettingtoknowyou.html');
+    }
+}
+
+if ($action == 'submit_gettingtoknowyou') {
+    $name = filter_input(INPUT_POST, 'name');
+    
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    
+    $major = filter_input(INPUT_POST, 'major');
+    
+    if ($major == NULL) {
+        $major = 'undecided';
+    }
+    
+    $places = filter_input(INPUT_POST, 'places', FILTER_REQUIRE_ARRAY);
+    
+    if ($name != NULL || $name != "" || $email != NULL || $email != "" || $major != NULL) {
+        require ('../views/whoyouare.html');
+    }
+}
