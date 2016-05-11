cb69b4eadc2ce879389e07a55f423d9de9c3ccbf
Meghan <shannon.mss@gmail.com>
2016-05-11 14:46:36 -0600

    added testdb.php, updated connect.php to not show anything

--- connect.php
+++ connect.php
@@ -1,18 +1,21 @@
 <?php 
 
-$dbHost = '';
-$dbPort = '';
-$dbUser = '';
-$dbPassword = '';
-$dbName = 'php';
+function dbConnect(){
+	$dbHost = '';
+	$dbPort = '';
+	$dbUser = '';
+	$dbPassword = '';
+	$dbName = 'php';
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser";
+	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser" ;
 
-$db = newPDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	$db = newPDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		return $db;
+}
 
 ?>
