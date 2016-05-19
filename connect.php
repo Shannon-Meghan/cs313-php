a51e8d79aebc13589d5583749788cf8c107dc1d4
Meghan <shannon.mss@gmail.com>
2016-05-18 20:12:52 -0600

    trying to get website to work correctly

--- connect.php
+++ connect.php
@@ -1,21 +1,41 @@
-<?php 
+<?php
 
-function dbConnect(){
-	$dbHost = '';
-	$dbPort = '';
-	$dbUser = '';
-	$dbPassword = '';
-	$dbName = 'php';
+function dbConnect() {
+    $dbHost = '';
+    $dbPort = '';
+    $dbUser = '';
+    $dbPassword = '';
+    $dbName = 'php';
 
-	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    $onOpenShift = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
-	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser" ;
+    if ($onOpenShift == null || $onOpenShift == "") {
+        //in our localhost environment
+        //$dbHost = '127.0.0.1';
+        //$dbPort = '80';
+        //$dbUser = 'root';
+        //$dbPassword = 'unicorn42';
+        
+        $dbHost = '127.0.0.1';
+        $dbPort = '80';
+        $dbUser = 'fandomta_client';
+        $dbPassword = 'o8DOv19~h!kh';
+        
+        echo ("You are on a local host");
+    } else {
+        //in our OpenShift environment
+        $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+        $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+        $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+          
+    }
 
-	$db = newPDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		return $db;
+    //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser" ;
+
+    $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    
+    return $db;
 }
 
 ?>
